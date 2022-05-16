53f97ba2773e7800dc3e0ea1e46432c81433d878
Dayton-IX <max.dayton@protonmail.com>
2022-05-16 10:39:43 -0600

    hello world route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 'use strict';
 
-const Hapi = require('hapi');
+const Hapi = require('@hapi/hapi');
 
 const init = async () => {
     const server = Hapi.server({
         port: '3000',
         host: 'localhost'
     });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Hello, world!';
+        }
+    })
 
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
